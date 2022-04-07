27093cd5696dffc8e4bbced0d6cb69c21d311174
Erhan Varlık <erhanvarlik@marun.edu.tr>
2022-04-07 10:13:57 +0300

    Delete secret.py

--- secret.py
+++
@@ -1,6 +0,0 @@
-def returnUrl():
-    #return "postgres://pylafsgesfibkp:008aa6f8817e256b98e034493c344833da4b0447a1d11fb05073ea4ec87447ff@ec2-35-153-88-219.compute-1.amazonaws.com:5432/db0rfao3q1lr16"
-    return "postgres://postgres:123456@localhost:5432/recipeDB"
-
-def returnSecret():
-    return "GBbuTbJDPZG4GzAqrn2j98a9NAHTEM7PFEvzBA8VFtazqfVAepw6YrDhTMzFbZS6"
