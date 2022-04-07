48d4636866a2e1492795defcc097e35521055611
Erhan Varlık <erhanvarlik@marun.edu.tr>
2022-04-07 10:15:57 +0300

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,5 @@
+def returnUrl():
+    return "postgres://postgres:123456@localhost:5432/recipeDB"
+
+def returnSecret():
+    return "GBbuTbJDPZG4GzAqrn2j98a9NAHTEM7PFEvzBA8VFtazqfVAepw6YrDhTMzFbZS6" 
